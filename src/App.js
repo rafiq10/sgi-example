447a74fabc8887ba00c2cbdb184590571b2be19d
Rafal Bil <tf05079@servidor.tis>
2018-05-24 13:20:19 +0200

    First Class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Posts from './Posts/Posts';
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to React</h1>
+        <Posts />
       </div>
     );
   }
---
+++ src/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Posts extends React.Component{
+
+  render(){
+
+    return(
+      <div>
+        <h1>Beinvenido equipo SGI</h1>
+      </div>
+    )
+  }
+}
