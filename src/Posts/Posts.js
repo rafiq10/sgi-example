5a0214b712d062568aecb986ead75714331b44d3
Rafal Bil <tf05079@servidor.tis>
2018-05-24 13:31:14 +0200

    adding axios

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Posts extends React.Component{
 
+  state={
+    posts:[]
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts').then( res => {
+      this.setState({posts: res.data.slice(1,11)})
+      console.log(this.state)
+    })
+  }
   render(){
 
     return(
       <div>
         <h1>Beinvenido equipo SGI</h1>
+
       </div>
     )
   }
